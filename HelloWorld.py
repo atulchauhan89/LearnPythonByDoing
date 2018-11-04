8ab7abd95c4fec2ae398039f819b91e3f6254358
atulchauhan89 <atul6101989@gmail.com>
2018-11-04 12:29:58 +0100

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+"""
+Let's start with Hello World program as a bignner.
+Python source files use the ".py" extension we called them as "modules". 
+"""
+
+# import is used to import any module. We will import sys module,which is a standerd module.
+
+import sys
+
+# Create a main() function for stucturing our code.
+def main():
+    print("Hello, World!")
+
+# Standard boilerplate to call the main() function to begin the program.    
+if __name__ == '__main__':
+    main()
