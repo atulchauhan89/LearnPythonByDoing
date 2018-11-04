b150ab4eee317b1f777f00a63e895f7db0b5bf7e
atulchauhan89 <atul6101989@gmail.com>
2018-11-04 12:38:55 +0100

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,7 +3,12 @@ Let's start with Hello World program as a bignner.
 Python source files use the ".py" extension we called them as "modules". 
 """
 
-# import is used to import any module. We will import sys module,which is a standerd module.
+"""
+import is used to import any module. We will import sys module,which is a standerd module.
+Modules are pre written code in Python.As everybody who is coding in python need same kind of functionality in there programs,
+So many Python comes with idea of Module. 
+Module are basically code or programs created by other developer to help the community.
+"""
 
 import sys
 
